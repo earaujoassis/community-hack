e88ab81afa91e7f859c83920515b7657c72fdefc
Carlos A <593456+earaujoassis@users.noreply.github.com>
2019-12-14 16:56:28 -0300

    remove header name

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -5,7 +5,6 @@ import './style.css';
 const header = () => {
   return (
     <div role="header" className="header-root">
-      <h1 className="header-title">COMMUNITY HACK</h1>
     </div>
   );
 };
