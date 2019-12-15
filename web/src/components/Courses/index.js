0b6d088ed0c28ac50ccf204f076fdb041f521f87
Carlos A <593456+earaujoassis@users.noreply.github.com>
2019-12-14 22:48:29 -0300

    add content for the courses cards

--- web/src/components/Courses/index.js
+++ web/src/components/Courses/index.js
@@ -10,7 +10,18 @@ const courses = () => {
     <div className="courses-root">
       <Menu />
       <div className="menu-fix courses-container">
-        <Card />
+        <Card
+          title="Lógica de programação"
+          description="Dê seus primeiros passos de maneira prática! Inicie na programação no seu celular e mostre seus programas para seus colegas."
+        />
+        <Card
+          title="Crie a sua primeira aplicação móvel"
+          description="Entenda o que é plataforma web e aprenda a criar e desenvolver aplicações móveis, entendendo a teoria básica de construção."
+        />
+        <Card
+          title="Sua primeira página da Web"
+          description="Torne-se um programador Web: entenda o básico e desenvolva uma página Web completa."
+        />
       </div>
     </div>
   );
