0c93df791799e2bcbafa8b589f001037f1b95ef0
IFormiga <igorformigaa@gmail.com>
2019-12-15 10:03:04 -0300

    Terminando as 3 chamadas da api

--- api/AhGarra/Controllers/CourseController.cs
+++ api/AhGarra/Controllers/CourseController.cs
@@ -6,7 +6,6 @@ using AhGarra.Domain.Entities.DTO;
 using AhGarra.Domain.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AhGarra.Controllers
 {
@@ -21,14 +20,14 @@ namespace AhGarra.Controllers
     }
 
     [HttpGet]
-    [Route("/GetAllCoursesBasicInformation")]
+    [Route("GetAllCoursesBasicInformation")]
     public ActionResult GetAllCoursesBasicInformation()
     {
       return Ok(_courseService.GetAllCourseBasicInformation());
     }
 
     [HttpGet]
-    [Route("/GetCourseCardsAndQuiz/{idCourse}")]
+    [Route("GetCourseCardsAndQuiz/{idCourse}")]
     public ActionResult GetCourseCardsAndQuiz([FromRoute]int idCourse)
     {
       return Ok(_courseService.GetCourseCardsAndQuiz(idCourse));
@@ -37,7 +36,7 @@ namespace AhGarra.Controllers
     // TODO: Setar flag de concluído
 
     [HttpPut]
-    [Route("/SetCourseStatus/{isCompleted}/{idCourse}")]
+    [Route("SetCourseStatus/{isCompleted}/{idCourse}")]
     public ActionResult SetCourseStatus(bool isCompleted, int idCourse)
     {
       _courseService.SetCourseStatus(isCompleted, idCourse);
